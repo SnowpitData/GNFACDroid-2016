a41b8294de68f00e2b65362dd80e5a9bfd66713d
mark-kahrl <mark.kahrl@gmail.com>
2015-12-23 13:29:56 -0800

    Initial commit

---
+++ app/src/main/java/gnfac/app/VideoActivity.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)VideoActivity.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/6/12
+ */
+package gnfac.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class VideoActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setTitle(getResources().getString(R.string.video_title));
+        setContentView(R.layout.web_view);
+        WebView wv = (WebView)findViewById(R.id.webview);
+       /// wv.loadUrl("http://www.youtube.com/playlist?list=PLXu5151nmAvT7gHqdNajDBGfGY4u5XM");
+       /// wv.loadUrl("https://www.youtube.com/playlist?list=PLXu5151nmAvRs_cO_yoHbvaXfYOqzrzjK");
+        ////////
+        finish();
+    }
+}
